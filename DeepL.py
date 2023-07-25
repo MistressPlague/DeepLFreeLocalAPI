8e02a1c2a23dabfd54d14a82f7faa94d2c9afb33
Kanna <36628963+MistressPlague@users.noreply.github.com>
2023-07-25 23:12:34 +0100

    Update DeepL.py

--- DeepL.py
+++ DeepL.py
@@ -4,6 +4,7 @@ monkey.patch_all()
 
 import re
 import time
+import json
 
 from playwright.sync_api import sync_playwright
 from flask import Flask, request, jsonify
@@ -17,6 +18,10 @@ context = browser.new_context()
 page = context.new_page()
 page.goto("https://www.deepl.com/translator")
 
+def makejson(obj):
+    # Serialize the object to a JSON-formatted string
+    return json.dumps(obj, ensure_ascii=False, indent=2)
+
 @app.route('/translate', methods=['POST'])
 def translate_api():
     # Cache JSON Received
@@ -24,7 +29,7 @@ def translate_api():
 
     # Check JSON Validity
     if 'text' not in data or 'lang' not in data:
-        return jsonify({'error': 'Invalid JSON data'}), 400
+        return makejson({'error': 'Invalid JSON data'}), 400
     
     # Cache Values
     input_text = data['text']
@@ -32,17 +37,17 @@ def translate_api():
 
     # Check If Any Text Was Given# Check If Any Text Was Given
     if not input_text:
-        return jsonify({'error': 'Empty input text'}), 400
+        return makejson({'error': 'Empty input text'}), 400
     
     # Check If Any Lang Was Given
     if not lang:
-        return jsonify({'error': 'Empty lang'}), 400
+        return makejson({'error': 'Empty lang'}), 400
     
     # Translate The Text According To The Provided Lang
     translated_text = translate(input_text, lang)
 
     # Return The Result Back To The Client
-    return jsonify({'translated_text': translated_text}), 200
+    return makejson({'translated_text': translated_text}), 200
 
 def translate(input_text: str, lang: str) -> str:
     # Select The Target Language
@@ -64,6 +69,7 @@ def translate(input_text: str, lang: str) -> str:
         time.sleep(1)
 
     # Return Translation
+    print(outputbox.inner_text())
     return outputbox.inner_text()
 
 app.run()
