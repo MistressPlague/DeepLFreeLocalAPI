e5b6b00d4c6478aee6d01c0202c94a236c16f3ad
Kanna <36628963+MistressPlague@users.noreply.github.com>
2023-09-13 19:55:04 +0100

    This is annoying
    Randomly, the target lang was becoming german due to the website going splat, this sets the target lang after setting the target lang again, potentially wasting a translation request for stability

--- DeepL.py
+++ DeepL.py
@@ -12,7 +12,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 playwright = sync_playwright().start()
-browser = playwright.chromium.launch(headless=True)
+browser = playwright.chromium.launch(headless=False)
 context = browser.new_context()
 
 page = context.new_page()
@@ -50,11 +50,6 @@ def translate_api():
     return makejson({'translated_text': translated_text}), 200
 
 def translate(input_text: str, lang: str) -> str:
-    # Select The Target Language
-    if page.get_by_test_id("translator-target-lang").get_attribute("dl-selected-lang") != lang:
-        page.get_by_role("button", name=re.compile("Select target language", re.IGNORECASE)).click()
-        page.get_by_test_id("translator-lang-option-" + lang).click()
-
     # Fill In Our Text For Translating, Caching For Optimization, Filling Blank First For While Loop Later To Work
     sourcebox = page.get_by_role("textbox", name="Source text")
     sourcebox.click()
@@ -63,6 +58,15 @@ def translate(input_text: str, lang: str) -> str:
 
     # Cache Translation Output TextBox
     outputbox = page.get_by_test_id("translator-target-input")
+
+    # Wait For Translation To Finish By Waiting For Length > 0 And It Not To Be The Empty Newline DeepL Initializes It With
+    while len(outputbox.inner_text()) == 0 or outputbox.inner_text() == '\n':
+        time.sleep(1)
+    
+    # Select The Target Language
+    if page.get_by_test_id("translator-target-lang").get_attribute("dl-selected-lang") != lang and page.get_by_test_id("translator-target-lang").get_attribute("dl-selected-lang") != "en-EN":
+        page.get_by_role("button", name=re.compile("Select target language", re.IGNORECASE)).click()
+        page.get_by_test_id("translator-lang-option-" + lang).click()
 
     # Wait For Translation To Finish By Waiting For Length > 0 And It Not To Be The Empty Newline DeepL Initializes It With
     while len(outputbox.inner_text()) == 0 or outputbox.inner_text() == '\n':
